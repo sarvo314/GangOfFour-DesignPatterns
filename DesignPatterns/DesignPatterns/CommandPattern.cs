24dcd2aadd72b323343ae3149c43176c2542133d
Sarvottam <sarvottamswaroop100@gmail.com>
2023-10-20 13:08:46 +0530

    Added a command pattern file

---
+++ DesignPatterns/DesignPatterns/CommandPattern.cs
@@ -0,0 +1,138 @@
+﻿using System;
+namespace DesignPatterns
+{
+    public interface Command
+    {
+        public void execute();
+    }
+
+    class Light
+    {
+
+        public void on()
+        {
+            Console.WriteLine("Light is on");
+        }
+
+        public void off()
+        {
+            Console.WriteLine("Light is off");
+        }
+    }
+
+    class LightOnCommand : Command
+    {
+        Light light;
+
+        public LightOnCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void execute()
+        {
+            light.on();
+        }
+    }
+
+
+    class LightOffCommand : Command
+    {
+        Light light;
+
+        public LightOffCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void execute()
+        {
+            light.off();
+        }
+    }
+
+    public class Stereo
+    {
+        public void on()
+        {
+            Console.WriteLine("Stereo is on");
+        }
+
+        public void off()
+        {
+            Console.WriteLine("Stereo is off");
+        }
+
+        public void setVolume(int volume)
+        {
+            Console.WriteLine("Stereo volume set to " + volume);
+        }
+
+    }
+    public class StereoOffCommand : Command
+    {
+        Stereo stereo;
+
+        public StereoOffCommand(Stereo stereo)
+        {
+            this.stereo = stereo;
+        }
+        public void execute()
+        {
+            stereo.off();
+        }
+    }
+    public class StereoOnCommand : Command
+    {
+        Stereo stereo;
+
+        public StereoOnCommand(Stereo stereo)
+        {
+            this.stereo = stereo;
+        }
+        public void execute()
+        {
+            stereo.on();
+        }
+    }
+
+    public class SimpleRemoteControl
+    {
+        Command slot;
+
+        public void setCommand(Command command)
+        {
+            slot = command;
+        }
+
+        public void buttonWasPressed()
+        {
+            slot.execute();
+        }
+    }
+
+    public class CommandPattern
+    {
+        public CommandPattern()
+        {
+
+            // Console.Write("eh");
+        }
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("hsd");
+            SimpleRemoteControl remote = new SimpleRemoteControl();
+            Light light = new Light();
+            Stereo stereo = new Stereo();
+
+            remote.setCommand(new LightOnCommand(light));
+            remote.buttonWasPressed();
+
+            remote.setCommand(new StereoOnCommand(stereo));
+            remote.buttonWasPressed();
+            CommandPattern cp = new CommandPattern();
+
+        }
+    }
+}
+
---
+++ DesignPatterns/DesignPatterns/EmptyClass.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace DesignPatterns
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
+
